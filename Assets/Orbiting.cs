eac27480ed0761a53154fa83b5e82a2d11b87b16
Alper Ozaydin <aozaydi1@binghamton.edu>
2017-05-08 20:15:33 +0200

    exercise 1

---
+++ Assets/Orbiting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbiting : MonoBehaviour {
+
+	public Transform SunTarget;
+	public Transform Moon;
+	public Rigidbody rb;
+
+	void Awake () {
+		
+		Moon = transform;
+		rb.AddForce(transform.right * 100);
+		rb.AddForce(transform.up * -100);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+		GameObject Sun = GameObject.FindGameObjectWithTag("Sun");
+		SunTarget = Sun.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 line = SunTarget.position - Moon.position;
+		line.Normalize();
+
+		float distance = Vector3.Distance(SunTarget.position, Moon.position);
+		rb.AddForce(line * 10/distance);
+	}
+}
