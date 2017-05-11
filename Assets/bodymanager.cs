064a61fc323d6666e882cf3df0184f730609f912
Alper Ozaydin <aozaydi1@binghamton.edu>
2017-05-11 02:10:24 +0200

    Added n body physic

---
+++ Assets/bodymanager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bodymanager : MonoBehaviour {
+
+
+	public GameObject[] body;
+	public GameObject[] body2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void awake(){
+		body = GameObject.FindGameObjectsWithTag("Sun");
+		body2 = GameObject.FindGameObjectsWithTag("Earth");
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		body = GameObject.FindGameObjectsWithTag("Sun");
+		body2 = GameObject.FindGameObjectsWithTag("Earth");
+	}
+}
